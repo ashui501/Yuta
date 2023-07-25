ce688f1f8c5d0f241d876fb1e5f9d46e7825b051
Anonymous <adi855053@gmail.com>
2023-07-25 22:45:33 +0530

    Update pyrogram.py

--- YutaRobot/services/pyrogram.py
+++ YutaRobot/services/pyrogram.py
@@ -2,7 +2,7 @@ import logging
 
 from pyrogram import Client
 #from pyromod import listen
-from GabiBraunRobot.conf import get_int_key, get_str_key
+from YutaRobot.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 APP_ID = get_int_key("APP_ID", required=True)
