3e254be0d8e4b29d8cad79ec180f918d69b210e3
Anonymous <adi855053@gmail.com>
2023-07-26 00:10:37 +0530

    Update sed.py

--- YutaRobot/modules/sed.py
+++ YutaRobot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from GabiBraunRobot import LOGGER, dispatcher
-from GabiBraunRobot.modules.disable import DisableAbleMessageHandler
-from GabiBraunRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from YutaRobot import LOGGER, dispatcher
+from YutaRobot.modules.disable import DisableAbleMessageHandler
+from YutaRobot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters
 
