9a0e1b9a48c5c552b25e3d1af2b3c38c5965ce89
Anonymous <adi855053@gmail.com>
2023-07-25 22:51:31 +0530

    Update extraction.py

--- YutaRobot/modules/helper_funcs/extraction.py
+++ YutaRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from GabiBraunRobot import LOGGER
-from GabiBraunRobot.modules.users import get_user_id
+from YutaRobot import LOGGER
+from YutaRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
