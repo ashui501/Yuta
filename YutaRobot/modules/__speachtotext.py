432afa7545dc5b12d01f349d0d27641d07842b02
Anonymous <adi855053@gmail.com>
2023-07-25 23:22:51 +0530

    Update __speachtotext.py

--- YutaRobot/modules/__speachtotext.py
+++ YutaRobot/modules/__speachtotext.py
@@ -11,8 +11,8 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from GabiBraunRobot import *
-from GabiBraunRobot.events import register
+from YutaRobot import *
+from YutaRobot.events import register
 
 
 async def is_register_admin(chat, user):
