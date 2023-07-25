542d5cc5bf01a8696bac3cffacf86bdfec42e8f0
Anonymous <adi855053@gmail.com>
2023-07-25 22:45:47 +0530

    Update telethon.py

--- YutaRobot/services/telethon.py
+++ YutaRobot/services/telethon.py
@@ -1,7 +1,7 @@
 
 from telethon import TelegramClient
 
-from GabiBraunRobot.conf import get_int_key, get_str_key
+from YutaRobot.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 NAME = TOKEN.split(":")[0]
