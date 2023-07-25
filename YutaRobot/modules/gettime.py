9ba0f0227ca90374b67dd28161481d9fbd197135
Anonymous <adi855053@gmail.com>
2023-07-25 23:54:26 +0530

    Update gettime.py

--- YutaRobot/modules/gettime.py
+++ YutaRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from GabiBraunRobot import TIME_API_KEY, dispatcher
-from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from YutaRobot import TIME_API_KEY, dispatcher
+from YutaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
