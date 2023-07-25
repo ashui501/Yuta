f3278b8e3f16e96889c904c8ff487180ba37a506
Anonymous <adi855053@gmail.com>
2023-07-26 00:15:39 +0530

    Update wallpaper.py

--- YutaRobot/modules/wallpaper.py
+++ YutaRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from GabiBraunRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from YutaRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from YutaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 
