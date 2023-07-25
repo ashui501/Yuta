3fb3ae257287f9410bc2cf3e83a8ab973cde78db
Anonymous <adi855053@gmail.com>
2023-07-25 22:54:21 +0530

    Update __init__.py

--- YutaRobot/modules/sql/__init__.py
+++ YutaRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from GabiBraunRobot import DB_URI
+from YutaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
