297d29a4f74ebfd1bfd8c8e72c8ad4f9d6586508
Anonymous <adi855053@gmail.com>
2023-07-26 00:08:16 +0530

    Update remote_cmds.py

--- YutaRobot/modules/remote_cmds.py
+++ YutaRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from GabiBraunRobot import dispatcher
-from GabiBraunRobot.modules.helper_funcs.chat_status import (
+from YutaRobot import dispatcher
+from YutaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from GabiBraunRobot.modules.helper_funcs.extraction import extract_user_and_text
-from GabiBraunRobot.modules.helper_funcs.filters import CustomFilters
+from YutaRobot.modules.helper_funcs.extraction import extract_user_and_text
+from YutaRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
