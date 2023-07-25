6c6b0b7f7f8e094ef3212b01ae120bdd01e83464
Anonymous <adi855053@gmail.com>
2023-07-25 22:59:31 +0530

    Update locks_sql.py

--- YutaRobot/modules/sql/locks_sql.py
+++ YutaRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from GabiBraunRobot.modules.sql import SESSION, BASE
+from YutaRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
