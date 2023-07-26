ecf9136cdd262d245d91517ee3dd178c22d1ef61
Anonymous <adi855053@gmail.com>
2023-07-26 09:09:40 +0530

    Update memify.py

--- YutaRobot/modules/memify.py
+++ YutaRobot/modules/memify.py
@@ -39,7 +39,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./GabiBraunRobot/resources/default.ttf"
+        fnt = "./YutaRobot/resources/default.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
